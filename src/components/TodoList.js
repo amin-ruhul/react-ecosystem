ddb8fce8209b93cb2e9c2b66fb3dca501bfaa088
amin_ruhul <ruhulinfo50@gmail.com>
2021-06-24 21:47:51 +0600

    create thunk

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,16 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import TodoListItem from "./TodoListItem";
 import NewTodo from "./NewTodo";
+import { displayAlert } from "../thunks";
 import { removeTodo, completeTodo } from "../actions";
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletePress }) => (
+const TodoList = ({
+  todos = [],
+  onRemovePressed,
+  onCompletePress,
+  onDisplayAlertClicked,
+}) => (
   <div>
     <NewTodo />
     {todos.map((todo) => (
       <TodoListItem
         todo={todo}
         onRemovePressed={onRemovePressed}
-        onCompletePress={onCompletePress}
+        onCompletePress={onDisplayAlertClicked}
       />
     ))}
   </div>
@@ -24,6 +30,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
   onCompletePress: (text) => dispatch(completeTodo(text)),
+  onDisplayAlertClicked: () => dispatch(displayAlert),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
